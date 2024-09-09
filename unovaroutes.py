74966669abfaa1ef985a3cec716f42d2d2617e2a
Anthony Chappetto <89417843+AnthonyChappetto@users.noreply.github.com>
2024-09-09 16:38:51 -0500

    Update unovaroutes.py
    Added routes 4-7

--- unovaroutes.py
+++ unovaroutes.py
@@ -1,4 +1,4 @@
-from shared import get_version
+from shared import get_version, grab_users_month
 
 class Black:
 
@@ -65,20 +65,124 @@ class Black:
     
     @staticmethod
     def route4():
-        return
-    
+        current_route = "Route 4"
+        version = get_version()
+
+        match version:
+            case "black" | "white":
+                route_pokemon = {
+                    "Sandile": 40, "Darumaka": 40, "Scraggy": 20
+                }
+            case "black2":
+                route_pokemon = {
+                    "Sandile": 35, "Darumaka": 35, "Trubbish": 25, "Scraggy": 5     
+                }
+            case "white2":
+                route_pokemon = {
+                    "Sandile": 35, "Darumaka": 35, "Minccino": 25, "Scraggy": 5
+                }
+            case _:
+                print("Should be impossible to reach here.")
+
+        from unovamain import Unova
+        return Unova.encounter_pokemon(route_pokemon)
     @staticmethod
     def route5():
-        return
-    
+        current_route = "Route 5"
+        version = get_version()
+
+        match version:
+            case "black" | "black2":
+                route_pokemon = {
+                    "Gothita": 30, "Minccino": 30, "Trubbish": 20, "Liepard": 20
+                }
+            case "white" | "white2":
+                route_pokemon = {
+                    "Solosis": 30, "Minccino": 30, "Trubbish": 20, "Liepard": 20
+                }
+            case _:
+                print("Should be impossible to reach here.")
+                
+        from unovamain import Unova
+        return Unova.encounter_pokemon(route_pokemon)
     @staticmethod
     def route6():
-        return
-    
+        current_route = "Route 6"
+        version = get_version()
+        season = grab_users_month()
+
+        if season in ["spring", "summer", "fall"]:
+            match version:
+                case "black" | "white":
+                    route_pokemon = {
+                        "Deerling": 35, "Karrablast": 25, "Tranquill": 15,
+                        "Foongus": 15, "Swadloon": 10
+                    }
+                case "black2":
+                    route_pokemon = {
+                        "Deerling": 30, "Shelmet": 25, "Tranquill": 15,
+                        "Foongus": 10, "Swadloon": 10, "Marill": 5, "Karrablast": 5
+                    }
+                case "white2":
+                    route_pokemon = {
+                        "Deerling": 30, "Karrablast": 25, "Tranquill": 15,
+                        "Foongus": 10, "Swadloon": 10, "Marill": 5, "Shelmet": 5
+                    }
+        else: #Winter
+            match version:
+                case "black" | "white":
+                    route_pokemon = {
+                        "Deeling": 35, "Karrablast": 25, "Vanillite": 15,
+                        "Foongus": 15, "Swadloon": 10
+                    }
+                case "black2":
+                    route_pokemon = {
+                        "Deering": 30, "Shelmet": 25, "Tranquill": 15,
+                        "Foongus": 10, "Swadloon": 10, "Marill": 5, "Karrablast": 5
+                    }
+                case "white2":
+                    route_pokemon = {
+                        "Deering": 30, "Shelmet": 25, "Tranquill": 15,
+                        "Foongus": 10, "Swadloon": 10, "Marill": 5, "Karrablast": 5
+                    }
+
+        from unovamain import Unova
+        return Unova.encounter_pokemon(route_pokemon)    
     @staticmethod
     def route7():
-        return
-    
+        current_route = "Route 7"
+        version = get_version()
+        season = grab_users_month()
+
+        if season in ["spring", "summer", "fall"]:
+            match version:
+                case "black" | "white":
+                    route_pokemon = {
+                        "Tranquill": 30, "Deerling": 20, "Watchog": 20,
+                        "Zebstrika": 20, "Foongus": 10
+                    }
+                case "black2" | "white2":
+                    route_pokemon = {
+                        "Tranquill": 30, "Deerling": 20, "Zebstrika": 20,
+                        "Watchog": 15, "Zangoose": 5, "Seviper": 5,
+                        "Foongus": 5
+                    }
+        else: #Winter 
+            match version:
+                case "black" | "white":
+                    route_pokemon = {
+                        "Cubchoo": 30, "Deerling": 20, "Watchog": 20,
+                        "Zebstrika": 20, "Foongus": 10
+                    }
+                case "black2" | "white2":
+                    route_pokemon = {
+                        "Cubchoo": 25, "Deerling": 20, "Zebstrika": 20,
+                        "Watchog": 10, "Tranquill": 10, "Zangoose": 5, 
+                        "Seviper": 5, "Foongus": 5
+                    }
+                    
+        from unovamain import Unova
+        return Unova.encounter_pokemon(route_pokemon)
     @staticmethod
     def route8():
         return
