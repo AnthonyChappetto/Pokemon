8d177ce9f8d65226df550a9411f7c1e809d074c1
Anthony Chappetto <89417843+AnthonyChappetto@users.noreply.github.com>
2024-09-09 16:38:42 -0500

    month
    Added function that takes users month from their calendar to be used in unova seasons

--- shared.py
+++ shared.py
@@ -1,3 +1,5 @@
+from datetime import datetime 
+
 version = None
 
 def set_version(new_version):
@@ -21,3 +23,17 @@ def get_version():
         str: The current version of the game the user is playing.
     """
     return version
+
+def grab_users_month():
+    current_month = datetime.now().month
+
+    if current_month in [1, 5, 9]:
+        season = "spring"
+    elif current_month in [2, 6, 10]:
+        season = "summer"
+    elif current_month in [3, 7, 11]:
+        season = "fall"
+    else:
+        season = "winter"
+
+    return season
